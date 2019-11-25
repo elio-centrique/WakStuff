5093ec1527bd34a13f9ea1251748c9b1fb29d395
elio-centrique <delannel.alan@laposte.net>
2019-11-25 22:13:11 +0100

    refactoring + optimisations

--- Wakstuff.rb
+++ Wakstuff.rb
@@ -65,6 +65,162 @@ language = file_data[0]
 I18n.load_path << Dir[File.expand_path("locale") + "/*.yml"]
 I18n.default_locale = language.to_sym
 
+class Item    
+    def initialize(name, rarity, color, level, description, stats, image)
+        @name = name
+        @rarity = rarity
+        @color = color
+        @level = level
+        @description = description
+        @stats = stats
+        @image = image
+    end
+
+    def name
+        @name
+    end
+
+    def setName(name)
+        @name = name
+    end
+
+    def rarity
+        @rarity
+    end
+
+    def setRarity(rarity)
+        @rarity = rarity
+    end
+
+    def color
+        @color
+    end
+
+    def setColor(color)
+        @color = color
+    end
+
+    def level
+        @level
+    end
+
+    def setLevel(level)
+        @level = level
+    end
+
+    def description
+        @description
+    end
+
+    def setDescription(description)
+        @description = description
+    end
+
+    def getStats
+        message = ""
+        for stat in @stats
+            message += stat + "\n"
+        end
+        return message
+    end
+
+    def setStats(stats)
+        @stats = stats
+    end
+
+    def image
+        @image
+    end
+end
+
+listItems = []
+fileItem = File.open("JSON/items.json", "r")
+jsonItem = JSON.load(fileItem)
+fileAction = File.open("JSON/actions.json", "r")
+jsonAction = JSON.load(fileAction)
+
+def loadItemList(listItems, jsonItem, jsonAction, language)
+    puts "Creation of the item list, it could take some times"
+    for item in jsonItem
+        rarityNumber = item['definition']['item']['baseParameters']['rarity']
+        case rarityNumber
+        when 1
+            rarity = I18n.t(:common)
+            color = 16777215
+        when 2
+            rarity = I18n.t(:rare)
+            color = 65280
+        when 3
+            rarity = I18n.t(:mythic)
+            color = 16750592
+        when 4
+            rarity = I18n.t(:legendary)
+            color = 16776960
+        when 5
+            rarity = I18n.t(:relic)
+            color = 11075839
+        when 6
+            rarity = I18n.t(:souvenir)
+            color = 52991
+        when 7
+            rarity = I18n.t(:epic)
+            color = 16711935
+        end
+        tmpStats = []
+        for bonus in item['definition']['equipEffects']
+            bonusId = bonus['effect']['definition']['actionId']
+            for action in jsonAction
+                if bonusId === action['definition']['id']
+                    m = ""
+                    param = bonus['effect']['definition']['params']
+                    level = item['definition']['item']['level'].to_i
+                    if (action['description'] != nil)
+                        m = action['description'][language].encode("ISO-8859-1").encode('utf-8')
+                        m.gsub! '[~3]?[#1] Maîtrise [#3]:', ""
+                        m.gsub! '[~3]?[#1] Mastery [#3]:', ""
+                        m.gsub! '[~3]?[#1] Résistance [#3]:', ""
+                        m.gsub! '[~3]?[#1] Resistance [#3]:', ""
+                        m.gsub! '[#1]', (param[1].to_i * level + param[0].to_i).to_s
+                        m.gsub! '[#2]', (param[3].to_i * level + param[2].to_i).to_s
+                        m.gsub! '[#3]', (param[5].to_i * level + param[4].to_i).to_s
+                        m.gsub! '[>1]?', ""
+                        m.gsub! '{[>2]?:s}', "s"
+                        m.gsub! '{[>2]?s:}', "s"
+                        m.gsub! '{[=2]?:s}', ""
+                        m.gsub! '{[=2]?s:}', ""
+                        m.gsub! '{[=2]?:}', ""
+                        m.gsub! '[~3]?', ""
+                        m.gsub! "{", ""
+                        m.gsub! "}", ""
+                        m.gsub! '[el1]', I18n.t(:fire)
+                        m.gsub! '[el2]', I18n.t(:water)
+                        m.gsub! '[el3]', I18n.t(:earth)
+                        m.gsub! '[el4]', I18n.t(:air)
+                        tmpStats.push m
+                    end
+                end
+            end
+        end
+        description = ""
+        if (item['description'] != nil)
+            description = "*" + item['description'][language] + "*"
+        end
+        tmpItem = Item.new(
+            item['title'][language], 
+            rarity,
+            color,
+            item['definition']['item']['level'],
+            description,
+            tmpStats,
+            "https://s.ankama.com/www/static.ankama.com/wakfu/portal/game/item/115/" + item['definition']['item']['graphicParameters']['gfxId'].to_s + ".png"
+        )
+        listItems.push tmpItem
+    end
+    puts "Itemlist finished, enjoy the bot !"
+end
+
+loadItemList(listItems, jsonItem, jsonAction, language)
+
 bot = Discordrb::Commands::CommandBot.new token: token[0], prefix: "w!", advanced_functionality: true
 
 bot.command(:almanax, max_args: 0, description: I18n.t(:almanaxCommand)) do |event|
@@ -101,73 +257,18 @@ bot.command(:object, min_args: 2, description: I18n.t(:objectCommand)) do |event
         moreArgs = args[1]
     end
 
-    for item in jsonItem
-        if moreArgs.downcase === item['title'][language].downcase
+    for item in listItems
+        if moreArgs.downcase === item.name.downcase
             findObject = true
-            level = item['definition']['item']['level']
-            rarityNumber = item['definition']['item']['baseParameters']['rarity']
-            case rarityNumber
-            when 1
-                rarity = I18n.t(:common)
-                color = 16777215
-            when 2
-                rarity = I18n.t(:rare)
-                color = 65280
-            when 3
-                rarity = I18n.t(:mythic)
-                color = 16750592
-            when 4
-                rarity = I18n.t(:legendary)
-                color = 16776960
-            when 5
-                rarity = I18n.t(:relic)
-                color = 11075839
-            when 6
-                rarity = I18n.t(:souvenir)
-                color = 52991
-            when 7
-                rarity = I18n.t(:epic)
-                color = 16711935
-            end
             messageEmbed = ""
-            if args[0].downcase == rarity.downcase
+            if args[0].downcase == item.rarity.downcase
                 findRarity = true
-                itemlink = "https://s.ankama.com/www/static.ankama.com/wakfu/portal/game/item/115/" + item['definition']['item']['graphicParameters']['gfxId'].to_s + ".png"
-                for bonus in item['definition']['equipEffects']
-                    bonusId = bonus['effect']['definition']['actionId']
-                    for action in jsonAction
-                        if bonusId === action['definition']['id']
-                            param = bonus['effect']['definition']['params']
-                            level = item['definition']['item']['level'].to_i
-                            m = action['description'][language].encode("ISO-8859-1").encode('utf-8')
-                            m.gsub! '[~3]?[#1] Maîtrise [#3]:', ""
-                            m.gsub! '[~3]?[#1] Mastery [#3]:', ""
-                            m.gsub! '[#1]', (param[1].to_i * level + param[0].to_i).to_s
-                            m.gsub! '[#2]', (param[3].to_i * level + param[2].to_i).to_s
-                            m.gsub! '[#3]', (param[5].to_i * level + param[4].to_i).to_s
-                            m.gsub! '[>1]?', ""
-                            m.gsub! '{[>2]?:s}', "s"
-                            m.gsub! '{[>2]?s:}', "s"
-                            m.gsub! '{[=2]?:s}', ""
-                            m.gsub! '{[=2]?s:}', ""
-                            m.gsub! '{[=2]?:}', ""
-                            m.gsub! '[~3]?', ""
-                            m.gsub! "{", ""
-                            m.gsub! "}", ""
-                            m.gsub! '[el1]', I18n.t(:fire)
-                            m.gsub! '[el2]', I18n.t(:water)
-                            m.gsub! '[el3]', I18n.t(:earth)
-                            m.gsub! '[el4]', I18n.t(:air)
-                            messageEmbed += m + "\n"
-                        end
-                    end
-                end
                 event.send_embed do |embed|
-                    embed.title = moreArgs + " " +I18n.t(:level) + " " + level.to_s
-                    embed.description = messageEmbed
-                    embed.color = color
-                    embed.add_field(name: "Description: ", value: "*" + item['description'][language] + "*")
-                    embed.image = embed.image = Discordrb::Webhooks::EmbedImage.new(url: itemlink)
+                    embed.title = item.name + " " +I18n.t(:level) + " " + item.level.to_s
+                    embed.description = item.getStats
+                    embed.color = item.color
+                    embed.add_field(name: "Description: ", value: item.description)
+                    embed.image = Discordrb::Webhooks::EmbedImage.new(url: item.image)
                 end
             end
         end
@@ -193,78 +294,20 @@ bot.command(:compare, max_args: 4, description: I18n.t(:compareCommand)) do |eve
     findRarity1 = false
     findObject2 = false
     findRarity2 = false
-    level1 = ""
-    level2 = ""
-
 
     #item 1
-    for item in jsonItem
-        if args[1].downcase === item['title'][language].downcase
+    for item in listItems
+        if args[1].downcase === item.name.downcase
             findObject1 = true
-            level1 = item['definition']['item']['level']
-            rarityNumber = item['definition']['item']['baseParameters']['rarity']
-            case rarityNumber
-            when 1
-                rarity = I18n.t(:common)
-                color = 16777215
-            when 2
-                rarity = I18n.t(:rare)
-                color = 65280
-            when 3
-                rarity = I18n.t(:mythic)
-                color = 16750592
-            when 4
-                rarity = I18n.t(:legendary)
-                color = 16776960
-            when 5
-                rarity = I18n.t(:relic)
-                color = 11075839
-            when 6
-                rarity = I18n.t(:souvenir)
-                color = 52991
-            when 7
-                rarity = I18n.t(:epic)
-                color = 16711935
-            end
             messageEmbed = ""
-            if args[0].downcase == rarity.downcase
+            if args[0].downcase == item.rarity.downcase
                 findRarity1 = true
-                linkpart = item['definition']['item']['graphicParameters']['gfxId']
-                for bonus in item['definition']['equipEffects']
-                    bonusId = bonus['effect']['definition']['actionId']
-                    for action in jsonAction
-                        if bonusId === action['definition']['id']
-                            param = bonus['effect']['definition']['params']
-                            level = item['definition']['item']['level'].to_i
-                            m = action['description'][language].encode("ISO-8859-1").encode('utf-8')
-                            m.gsub! '[~3]?[#1] Maîtrise [#3]:', ""
-                            m.gsub! '[~3]?[#1] Mastery [#3]:', ""
-                            m.gsub! '[#1]', (param[1].to_i * level + param[0].to_i).to_s
-                            m.gsub! '[#2]', (param[3].to_i * level + param[2].to_i).to_s
-                            m.gsub! '[#3]', (param[5].to_i * level + param[4].to_i).to_s
-                            m.gsub! '[>1]?', ""
-                            m.gsub! '{[>2]?:s}', "s"
-                            m.gsub! '{[>2]?s:}', "s"
-                            m.gsub! '{[=2]?:s}', ""
-                            m.gsub! '{[=2]?s:}', ""
-                            m.gsub! '{[=2]?:}', ""
-                            m.gsub! '[~3]?', ""
-                            m.gsub! "{", ""
-                            m.gsub! "}", ""
-                            m.gsub! '[el1]', I18n.t(:fire)
-                            m.gsub! '[el2]', I18n.t(:water)
-                            m.gsub! '[el3]', I18n.t(:earth)
-                            m.gsub! '[el4]', I18n.t(:air)
-                            messageEmbed += m + "\n"
-                        end
-                    end
-                end
                 event.send_embed do |embed|
-                    embed.title = args[1] + " " + I18n.t(:level) + " " + level1.to_s
-                    embed.description = messageEmbed
-                    embed.color = color
-                    embed.add_field(name: "Description: ", value: "*" + item['description'][language] + "*")
-                    embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://s.ankama.com/www/static.ankama.com/wakfu/portal/game/item/115/" + linkpart.to_s + ".png")
+                    embed.title = item.name + " " +I18n.t(:level) + " " + item.level.to_s
+                    embed.description = item.getStats
+                    embed.color = item.color
+                    embed.add_field(name: "Description: ", value: item.description)
+                    embed.image = Discordrb::Webhooks::EmbedImage.new(url: item.image)
                 end
             end
         end
@@ -277,73 +320,18 @@ bot.command(:compare, max_args: 4, description: I18n.t(:compareCommand)) do |eve
     end
 
     #item 2
-    for item in jsonItem
-        if args[3].downcase === item['title'][language].downcase
+    for item in listItems
+        if args[3].downcase === item.name.downcase
             findObject2 = true
-            level2 = item['definition']['item']['level']
-            rarityNumber = item['definition']['item']['baseParameters']['rarity']
-            case rarityNumber
-            when 1
-                rarity = I18n.t(:common)
-                color = 16777215
-            when 2
-                rarity = I18n.t(:rare)
-                color = 65280
-            when 3
-                rarity = I18n.t(:mythic)
-                color = 16750592
-            when 4
-                rarity = I18n.t(:legendary)
-                color = 16776960
-            when 5
-                rarity = I18n.t(:relic)
-                color = 11075839
-            when 6
-                rarity = I18n.t(:souvenir)
-                color = 52991
-            when 7
-                rarity = I18n.t(:epic)
-                color = 16711935
-            end
             messageEmbed = ""
-            if args[2].downcase == rarity.downcase
+            if args[2].downcase == item.rarity.downcase
                 findRarity2 = true
-                linkpart = item['definition']['item']['graphicParameters']['gfxId']
-                for bonus in item['definition']['equipEffects']
-                    bonusId = bonus['effect']['definition']['actionId']
-                    for action in jsonAction
-                        if bonusId === action['definition']['id']
-                            param = bonus['effect']['definition']['params']
-                            level = item['definition']['item']['level'].to_i
-                            m = action['description'][language].encode("ISO-8859-1").encode('utf-8')
-                            m.gsub! '[~3]?[#1] Maîtrise [#3]:', ""
-                            m.gsub! '[~3]?[#1] Mastery [#3]:', ""
-                            m.gsub! '[#1]', (param[1].to_i * level + param[0].to_i).to_s
-                            m.gsub! '[#2]', (param[3].to_i * level + param[2].to_i).to_s
-                            m.gsub! '[#3]', (param[5].to_i * level + param[4].to_i).to_s
-                            m.gsub! '[>1]?', ""
-                            m.gsub! '{[>2]?:s}', "s"
-                            m.gsub! '{[>2]?s:}', "s"
-                            m.gsub! '{[=2]?:s}', ""
-                            m.gsub! '{[=2]?s:}', ""
-                            m.gsub! '{[=2]?:}', ""
-                            m.gsub! '[~3]?', ""
-                            m.gsub! "{", ""
-                            m.gsub! "}", ""
-                            m.gsub! '[el1]', I18n.t(:fire)
-                            m.gsub! '[el2]', I18n.t(:water)
-                            m.gsub! '[el3]', I18n.t(:earth)
-                            m.gsub! '[el4]', I18n.t(:air)
-                            messageEmbed += m + "\n"
-                        end
-                    end
-                end
                 event.send_embed do |embed|
-                    embed.title = args[3] + " " + I18n.t(:level) + " " + level2.to_s
-                    embed.description = messageEmbed
-                    embed.color = color
-                    embed.add_field(name: "Description: ", value: "*" + item['description'][language] + "*")
-                    embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://s.ankama.com/www/static.ankama.com/wakfu/portal/game/item/115/" + linkpart.to_s + ".png")
+                    embed.title = item.name + " " +I18n.t(:level) + " " + item.level.to_s
+                    embed.description = item.getStats
+                    embed.color = item.color
+                    embed.add_field(name: "Description: ", value: item.description)
+                    embed.image = Discordrb::Webhooks::EmbedImage.new(url: item.image)
                 end
             end
         end
@@ -357,12 +345,18 @@ bot.command(:compare, max_args: 4, description: I18n.t(:compareCommand)) do |eve
 end
 
 bot.command(:setLanguage, min_args: 1, max_args: 1, description: I18n.t(:setLanguageCommand)) do |event, *args|
-    conf_file = File.read('config.txt')
-    conf_data = conf_file.split
-    File.write('config.txt', args[0])
-    language = args[0]
-    I18n.default_locale = args[0]
-    event << I18n.t(:setLang1) + args[0] + I18n.t(:setLang2)
+    if(args[0] == "fr" or args[0] == "en")
+        conf_file = File.read('config.txt')
+        conf_data = conf_file.split
+        File.write('config.txt', args[0])
+        language = args[0]
+        I18n.default_locale = args[0]
+        event << I18n.t(:loadingItems)
+        loadItemList(listItems, jsonItem, jsonAction, language)
+        event << I18n.t(:setLang1) + args[0] + I18n.t(:setLang2)
+    else
+        event << I18n.t(:wrongLanguage)
+    end
 end
 
 bot.command(:version, max_args: 0, description: I18n.t(:versionCommand)) do |event|
